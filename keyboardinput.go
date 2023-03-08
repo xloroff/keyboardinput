73c55f0ed71e1fb8fc07096a80a3320e84049fdd
Pavel <p.grazhdankin@roseltorg.ru>
2023-03-08 11:46:43 +0300

    first commit

---
+++ keyboardinput.go
@@ -0,0 +1,28 @@
+package keyboardinput
+
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func GetFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	getInputFronKeyb, err := reader.ReadString('\n')
+	if err != nil {
+		error := errors.New("Что-то не то прилетело в ввод!")
+		return 0, error
+	}
+
+	getInputFronKeyb = strings.TrimSpace(getInputFronKeyb)
+	number, err := strconv.ParseFloat(getInputFronKeyb, 64)
+	if err != nil {
+		error := errors.New("Что-то не то при конвертации ввода в число!")
+		return 0, error
+	}
+
+	return number, nil
+
+}
